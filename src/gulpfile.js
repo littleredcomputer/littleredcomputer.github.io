e9a8c8ea9f0809454b313e5bc7638309a5906bb1
Colin Smith <colin.smith@gmail.com>
2016-04-07 22:54:34 -0700

    standard js convention

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,27 +1,21 @@
-'use strict';
+'use strict'
 
-var browserify = require('browserify');
-var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify')
+var gulp = require('gulp')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+// var gutil = require('gulp-util')
+// var uglify = require('gulp-uglify')
+var sourcemaps = require('gulp-sourcemaps')
 
-gulp.task('javascript', function() {
-  // var b = browserify({
-  //   entries: './js/odex-demo.js',
-  //   debug: true
-  // });
-  gutil.log('here');
+gulp.task('javascript', function () {
   browserify({
     entries: './js/odex-demo.js',
     standalone: 'odexdemo'
-  })
-      .bundle()
+  }).bundle()
       .pipe(source('odex-demo-bundle.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({loadMaps: true}))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('../public/js'));
-});
+      .pipe(gulp.dest('../public/js'))
+})
--- src/js/graph.js
+++ src/js/graph.js
@@ -1,83 +1,82 @@
-'use strict';
-var d3 = require('d3');
-var Graph = function(options) {
-  var self = this;
-  this.options = options;
-  var margin = { left: 30, right: 10, top: 5, bottom: 30 };
-  this.width = options.width - margin.left - margin.right;
-  this.height = options.height - margin.top - margin.bottom;
-  this.x = d3.scale.linear().range([0, this.width]);
-  this.y = d3.scale.linear().range([this.height, 0]);
-  this.xAxis = d3.svg.axis().scale(this.x).orient('bottom');
-  this.yAxis = d3.svg.axis().scale(this.y).orient('left');
+'use strict'
+var d3 = require('d3')
+var Graph = function (options) {
+  var self = this
+  this.options = options
+  var margin = { left: 30, right: 10, top: 5, bottom: 30 }
+  this.width = options.width - margin.left - margin.right
+  this.height = options.height - margin.top - margin.bottom
+  this.x = d3.scale.linear().range([0, this.width])
+  this.y = d3.scale.linear().range([this.height, 0])
+  this.xAxis = d3.svg.axis().scale(this.x).orient('bottom')
+  this.yAxis = d3.svg.axis().scale(this.y).orient('left')
   this.svg = d3.select(options.element).append('svg')
     .attr('width', options.width)
     .attr('height', options.height)
     .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
   this.line = d3.svg.line()
-    .x(function(d) { return self.x(d[0]); })
-    .y(function(d) { return self.y(d[1]); });
-};
+    .x(function (d) { return self.x(d[0]) })
+    .y(function (d) { return self.y(d[1]) })
+}
 
-Graph.prototype.wrap_pi = function(angle) {
-  var pi = Math.PI;
-  var a = angle;
+Graph.prototype.wrap_pi = function (angle) {
+  var pi = Math.PI
+  var a = angle
   if (-pi > angle || angle >= pi) {
-    a = angle - 2 * pi * Math.floor(angle / 2.0 / pi);
-    a = a < pi ? a : a - 2 * pi;
+    a = angle - 2 * pi * Math.floor(angle / 2.0 / pi)
+    a = a < pi ? a : a - 2 * pi
   }
-  return a;
-};
+  return a
+}
 
-Graph.prototype.d0 = function(d) { return d[0]; };
-Graph.prototype.d1 = function(d) { return d[1]; };
+Graph.prototype.d0 = function (d) { return d[0] }
+Graph.prototype.d1 = function (d) { return d[1] }
 
-Graph.prototype.axesFromData = function(data) {
-  var that = this;
-  this.x.domain(d3.extent(data, d0));
+Graph.prototype.axesFromData = function (data) {
+  this.x.domain(d3.extent(data, this.d0))
   if (this.options.wrap_pi) {
-    this.y.domain([-Math.PI,Math.PI]);
+    this.y.domain([-Math.PI, Math.PI])
   } else {
-    this.y.domain(d3.extent(data, d1));
+    this.y.domain(d3.extent(data, this.d1))
   }
-  this.drawAxes();
+  this.drawAxes()
 }
 
 Graph.prototype.axes = function (xDomain, yDomain) {
-  this.x.domain(xDomain);
-  this.y.domain(yDomain);
-  this.drawAxes();
+  this.x.domain(xDomain)
+  this.y.domain(yDomain)
+  this.drawAxes()
 }
 
 Graph.prototype.drawAxes = function () {
   this.svg.append('g')
     .attr('class', 'x axis')
     .attr('transform', 'translate(0,' + this.height + ')')
-    .call(this.xAxis);
+    .call(this.xAxis)
   this.svg.append('g')
     .attr('class', 'y axis')
-    .call(this.yAxis);
-};
+    .call(this.yAxis)
+}
 
-Graph.prototype.draw = function(data, cls) {
-  var cls = cls || 'default';
-  var xf = this.options.wrap_pi ? this.wrap_pi : function(x) {return x;};
-  var self = this;
+Graph.prototype.draw = function (data, cls) {
+  cls = cls || 'default'
+  var xf = this.options.wrap_pi ? this.wrap_pi : function (x) { return x }
+  var self = this
   if (this.options.points) {
     this.svg.selectAll('circle.graph-point')
     .data(data)
     .enter()
     .append('circle')
-    .attr('cx', function(d) { return self.x(d[0]); })
-    .attr('cy', function(d) { return self.y(xf(d[1])); })
-    .attr('r', 1);
+    .attr('cx', function (d) { return self.x(d[0]) })
+    .attr('cy', function (d) { return self.y(xf(d[1])) })
+    .attr('r', 1)
   } else {
-    this.svg.selectAll('path.' + cls).remove();
+    this.svg.selectAll('path.' + cls).remove()
     this.svg.append('path').attr('class', cls)
       .datum(data)
       .classed({line: true, cls: true})
-      .attr('d', this.line);
+      .attr('d', this.line)
   }
-};
-exports.Graph = Graph;
+}
+exports.Graph = Graph
